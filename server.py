c225fa2f174532fc0cbdfd0b1cb767895e2eb170
robiboo <rb.panerio@gmail.com>
2024-11-06 21:34:18 -0500

    modified server.py to get 10/10 in pylint

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
+'''
+    These module is used to define routes for the Flask app
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("Emotion Detection")
+app = Flask(__name__)
 
 # Route to render the index page
 @app.route("/")
 def render_index_page():
-    return render_template('index.html') 
+    '''
+        This route returns the index.html for rendering
+    '''
+    return render_template('index.html')
 
 @app.route("/emotionDetector", methods=['GET'])
 def emotion_detection():
+    '''
+        This route the emotion_detection function.
+        Gets the textToAnalyze input and process it.
+        Returns the desired output
+    '''
 
     # Get the text to analyze from the query parameters (from URL)
     text_to_analyze = request.args.get("textToAnalyze")  # Guaranteed to be present
@@ -18,7 +29,7 @@ def emotion_detection():
     response = emotion_detector(text_to_analyze)
 
     # Return text if the dominant emotion is None
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
     # Extract the emotion scores and dominant emotion from the response
@@ -30,11 +41,10 @@ def emotion_detection():
     dominant_emotion = response['dominant_emotion']
 
     # Create the formatted result string
-    formatted_result = (
-        f"For the given statement, the system response is 'anger' : {anger_score}, 'disgust' : {disgust_score}, "
-        f"'fear' : {fear_score}, 'joy' : {joy_score} and 'sadness' : {sadness_score}. "
-        f"The dominant emotion is {dominant_emotion}."
-    )
+    formatted_result = f"For the given statement, the system response is 'anger': {anger_score}, "
+    formatted_result += f"'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and"
+    formatted_result += f" 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
+
     # Return the formatted result directly as the response
     return formatted_result
 
